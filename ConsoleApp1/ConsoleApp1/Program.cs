67de2e20a163724c377bcfa6a9978e0055195408
gulshanmammadova <gulshanfm@code.edu.az>
2022-09-03 16:06:42 +0400

    methodla daxil edilen sozu 10 defe yazmaq

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("sozu daxil edin:");
+            string word = Console.ReadLine();
+           HelloWorld(word);
+        }static void HelloWorld(string word)
+        {for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(word); ;
+            }
+        }
+    }
+}
